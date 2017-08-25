cb3fd58dc5d150ea0d0641ba9a03f552c367d5ff
kvburke <kvburke96@gmail.com>
2017-08-25 19:12:14 -0400

    updated.

--- WineShopFinal/src/main/java/com/example/demo/WebService.java
+++ WineShopFinal/src/main/java/com/example/demo/WebService.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 
@@ -17,18 +19,18 @@ public class WebService {
 	
 	
 	
-	
+	@Transactional
 	public  void addAccountService(int  account){
 		 DAO.addAccount(account);
 	}
 	
-
+	@Transactional
 	public List<AccountInventory> addAccountService(String  name){
 		return DAO.addAccount(name);
 	}
 	
 	
-	
+	@Transactional
 	public  void addUserService(String username, String password){
 		DAO.addUser(username, password);
 	}
@@ -37,19 +39,19 @@ public class WebService {
 	
 	
 	
-
+	@Transactional
 	public  ArrayList<Purchased> buyWineService(int id, int account, String username, String name, String brand, double price, int quantity, String url){	
 		return DAO.buyWine(id, account, username, name, brand, price, quantity, url);
 	}
 	
-	
+	@Transactional
 	public  ArrayList<Purchased> buyWineInventoryService(int id, int account, String username, String name, String brand, double price, int quantity,  String url){	
 		return DAO.buyWineInventory(id, account, username, name, brand, price, quantity, url);
 	}
 	
 	
 	
-	
+	@Transactional
 	public ArrayList<Purchased> removeWineService(int idx, int id, int account, String name, double price, int quantity,  String url, String brand)
 	{
 		return DAO.removeWine(idx, id, account, name, price, quantity, url, brand);
@@ -57,7 +59,7 @@ public class WebService {
 	
 	
 	
-	
+	@Transactional
 	public  ArrayList<Purchased> removeWineInventoryService(int id, int account, String name, double price, int quantity)
 	{
 		return DAO.removeWineInventory(id, account, name, price, quantity);	
@@ -65,33 +67,33 @@ public class WebService {
 	
 	
 	
-	
+	@Transactional
 	public List<WineInventory> displayInventoryService(){
 		return DAO.displayInventory();	
 	}
 	
 	
 	
-	
+	@Transactional
 	public  List<Wine> displayWinesService(int account){
 		return DAO.displayWines(account);
 	}
 	
 	
-	
+	@Transactional
 	public  List<Purchased> displayPurchasedService(int account){
 		return DAO.displayPurchased(account);
 	}
 	
 	
 	
-	
+	@Transactional
 	public  List<Total> getTotal(int account){
 		return DAO.getTotal(account);
 	}
 	
 	
-	
+	@Transactional
 	public  void buy(int account){
 		 DAO.buy(account);
 	}
